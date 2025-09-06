6b770ecccee5e462a3fd92d6dd6418595ead88e8
GauravPatel1202 <patelgaurav1202@gmail.com>
2025-09-07 01:24:50 +0530

    fixed

--- src/lending.tsx
+++ src/lending.tsx
@@ -10,7 +10,7 @@ const AILandingPage = () => {
   // Mouse glow effect
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   useEffect(() => {
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: any) => {
       setMousePosition({ x: e.clientX, y: e.clientY });
     };
     window.addEventListener("mousemove", handleMouseMove);
